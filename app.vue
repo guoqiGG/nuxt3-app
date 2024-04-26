dde3bbf23d2f95f637a1858662b4c094dd0bb0e2
guoqiGG <2411074379@qq.com>
2024-04-26 18:36:18 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup>
+const [{ data: organization }, { data: repos }] = await Promise.all([
+  useFetch(`https://api.github.com/orgs/nuxt`),
+  useFetch(`https://api.github.com/orgs/nuxt/repos`)
+])
+// console.log(organization.value,repos.value)
+const counter = useState('counter', () =>Math.round(Math.random() * 1000) )
+</script>
+
+<template>
+  <header>
+    <h1>{{ organization.login }}</h1>
+    <p>{{ organization.description }}</p>
+    <div>
+      Counter:{{counter}}
+      <button @click="counter++">+</button>
+      <button @click="counter++">-</button>
+    </div>
+  </header>
+</template>
